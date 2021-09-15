6fcbfb28c024b42ef5b0f4c33698b01e6c79f55d
johannamaria91 <johanna.agnar@outlook.com>
2021-09-15 11:04:13 +0200

    added static files

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express()
+const PORT = 8090
+
+app.use( '/', express.static(__dirname + '/../public') )
+app.use( '/img', express.static(__dirname + '/../hamsters') )
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}.`);
+})
